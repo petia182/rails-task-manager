4e038f09c6edf8c233ff7136ff4cf8cc21b7408d
Petya Nikolova <petia_182@yahoo.com>
2016-11-08 13:50:16 +0000

    gtgtgt

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+Task.create(name: 'tattoo', description: 'get a new tattoo', status: 'in progress')
+
+Task.create(name: 'shopping', description: 'grocery shopping', status: 'done')
+
+Task.create(name: 'cleaning', description: 'house cleaning', status: 'not started')
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
